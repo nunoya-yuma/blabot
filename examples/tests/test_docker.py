5594b8db1e52607bb7719802a84f0e3fb50b0aef
nunoya-yuma <nuno.yu.3838@gmail.com>
2024-09-21 22:46:14 +0900

    Add examples and tests related to docker

---
+++ examples/tests/test_docker.py
@@ -0,0 +1,69 @@
+from nn_io_interact.nn_io_interact.docker_io import DockerProcessIO
+import pytest
+import subprocess
+
+START_COMMAND = "python3 /app/example.py"
+DOCKER_IMAGE_NAME = "nn/example-app:latest"
+DOCKER_CONTAINER_NAME = "example_app"
+
+
+@pytest.fixture
+def docker_io_cli():
+    docker_io_cli = DockerProcessIO(
+        DOCKER_IMAGE_NAME,
+        DOCKER_CONTAINER_NAME,
+        True,
+        START_COMMAND)
+    docker_io_cli.start()
+    yield docker_io_cli
+    docker_io_cli.stop()
+
+
+@pytest.fixture
+def docker_io_exec_cli():
+    docker_run_command = [
+        "docker",
+        "run",
+        "-dit",
+        "--name",
+        DOCKER_CONTAINER_NAME,
+        DOCKER_IMAGE_NAME,
+        "bash"]
+    res_run = subprocess.run(docker_run_command)
+    assert res_run.returncode == 0, "Failed to run docker container"
+
+    docker_exec_command = ["docker", "exec", "-i", DOCKER_CONTAINER_NAME, "ls", "-a"]
+    res_ls = subprocess.run(docker_exec_command)
+    assert res_ls.returncode == 0, "Failed to run docker sample command"
+
+    docker_io_exec_cli = DockerProcessIO(
+        DOCKER_IMAGE_NAME,
+        DOCKER_CONTAINER_NAME,
+        False,
+        START_COMMAND)
+
+    docker_io_exec_cli.start()
+    yield docker_io_exec_cli
+    docker_io_exec_cli.stop()
+
+    docker_remove_command = [
+        "docker", "rm", "-f", DOCKER_CONTAINER_NAME
+    ]
+    res_remove = subprocess.run(docker_remove_command, stdout=subprocess.DEVNULL)
+    assert res_remove.returncode == 0, "Failed to remove docker container"
+
+
+@pytest.mark.docker_test
+def test_docker_simple(docker_io_cli):
+    assert docker_io_cli.wait_for("Initializing...")
+    assert docker_io_cli.wait_for("Complete startup sequence")
+
+    assert docker_io_cli.run_command("sample-ctrl on", "Sample status changed to 'on'")
+
+
+@pytest.mark.docker_test
+def test_docker_exec(docker_io_exec_cli):
+    assert docker_io_exec_cli.wait_for("Initializing...")
+    assert docker_io_exec_cli.wait_for("Complete startup sequence")
+
+    assert docker_io_exec_cli.run_command("sample-ctrl off", "Sample status changed to 'off'")
